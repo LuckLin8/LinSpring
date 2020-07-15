b9fb713daba3645c768cf0e0449d470526715963
LuckLin8 <goodlucklinlin@outlook.com>
2020-07-15 23:26:26 +0800

    添加support

--- src/main/java/com/lin/spring/ioc/support/BeanCreater.java
+++ src/main/java/com/lin/spring/ioc/support/BeanCreater.java
@@ -1,8 +1,32 @@
 package com.lin.spring.ioc.support;
 
+import com.lin.spring.ioc.BeanRegister;
+
+import java.util.List;
+
 /**
+ * 创建bean并且加入到容器工厂
+ *
+ *
  * @Date 2020/7/15 21:33
  * @Author lbw
  **/
 public class BeanCreater {
+
+    private BeanRegister register;
+
+    public BeanCreater(BeanRegister register) {
+        this.register = register;
+    }
+
+    public void create(List<BeanDefinition> bds) {
+        for (BeanDefinition bd : bds){
+            doCreate(bd);
+        }
+    }
+
+    private void doCreate(BeanDefinition bd) {
+        Object instance = bd.getInstance();
+        this.register.registInstanceMapping(bd.getId(),instance);
+    }
 }
---
+++ src/main/java/com/lin/spring/ioc/support/Populator.java
@@ -0,0 +1,8 @@
+package com.lin.spring.ioc.support;
+
+/**
+ * @Date 2020/7/15 21:44
+ * @Author lbw
+ **/
+public class Populator {
+}
